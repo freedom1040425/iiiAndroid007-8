0c143e2231e5717b17412f085da82c34cd27c9ea
Seraphim <freedom1040425@mail.com>
2019-11-12 16:48:35 +0800

    iiiAndroid007+008

---
+++ app/src/main/java/tw/org/iii/iiiandroid008/MyView.java
@@ -0,0 +1,26 @@
+package tw.org.iii.iiiandroid008;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class MyView extends View {
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setBackgroundColor(Color.BLUE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint= new Paint();
+        paint.setStrokeWidth(10);
+        canvas.drawLine(0,0,200,200,paint);
+    }
+}
