a9fc617389954e7389f61e74203e9a1e90fb22ea
Seraphim <freedom1040425@mail.com>
2019-11-15 11:22:56 +0800

    iiiAndroid007+008

--- app/src/main/java/tw/org/iii/iiiandroid008/MainActivity.java
+++ app/src/main/java/tw/org/iii/iiiandroid008/MainActivity.java
@@ -3,12 +3,28 @@ package tw.org.iii.iiiandroid008;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MyView myView; //宣告
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myView = findViewById(R.id.myView); //要跟前面XML連動
+
+    }
+    //執行動作
+    public void clearMyview(View view) {
+        myView.clear();
+    }
+
+    public void undo(View view) {
+        myView.undo();
+    }
+
+    public void redo(View view) {
+        myView.redo();
     }
 }
--- app/src/main/java/tw/org/iii/iiiandroid008/MyView.java
+++ app/src/main/java/tw/org/iii/iiiandroid008/MyView.java
@@ -17,11 +17,12 @@ import java.util.LinkedList;
                 //建構式
                 // 系統上自動產生 再去觸發 user 看到長相的邏輯是從這邊來 // 繼承原生的VIEW//畫面呈現
 public class MyView extends View {
-    private LinkedList<LinkedList<Point>> lines; //宣告新增物件
+    private LinkedList<LinkedList<Point>> lines,recycle; //宣告新增物件
         public MyView(Context context, @Nullable AttributeSet attrs){
             super(context, attrs);
             setBackgroundColor(Color.BLUE);//畫布顏色
             lines = new LinkedList<>(); //線的清單 給他初始直
+            recycle = new LinkedList<>(); //資源回收桶
         }
 
                 //一開始設定 初始化 按下去後觸發的事情 找出目的跟原因 建構式的構成
@@ -89,20 +90,43 @@ public class MyView extends View {
             }
 
 
-        /*for(int i=1; i<line.size();i++){ //每一點連起來
-           Point p0 =line.get(i-1);
-           Point p1 = line.get(i);
-           canvas.drawLine(p0.x,p0.y,p1.x,p1.y,paint);
-           }//剩下隨時都想看的到
-         */  //劃一條線
+                    /*for(int i=1; i<line.size();i++){ //每一點連起來
+                       Point p0 =line.get(i-1);
+                       Point p1 = line.get(i);
+                       canvas.drawLine(p0.x,p0.y,p1.x,p1.y,paint);
+                       }//剩下隨時都想看的到
+                     */  //劃一條線
+        }
+                        //類別是拿來生產物件
+                        //ex 汽車類別 汽車裏面有輪胎 買輪胎 才能動 別家公司輪胎(public)要能吻合汽車規格(參數傳遞)
+                        // 汽車種類 輪胎買不到 自己蓋了輪胎工廠在自己公司裡面 (這時候就沒有規格 自己講好就好 內部類別private (
+                        // 可以與外部類別直接溝通 可以直接呼叫))
+                        // 方便存取
+    }
+            //方法
+        public void clear(){
+        lines.clear();//線清掉
+        invalidate();//畫面清掉
 
         }
-        //類別是拿來生產物件
-        //ex 汽車類別 汽車裏面有輪胎 買輪胎 才能動 別家公司輪胎(public)要能吻合汽車規格(參數傳遞)
-        // 汽車種類 輪胎買不到 自己蓋了輪胎工廠在自己公司裡面 (這時候就沒有規格 自己講好就好 內部類別private (
-        // 可以與外部類別直接溝通 可以直接呼叫))
-        // 方便存取
-    }
+
+        public void  undo() {
+            if (lines.size() > 1) { //有線才丟掉
+                recycle.add(lines.removeLast());//最後那一步 並且丟進資源回收桶
+                invalidate();//畫面清掉
+
+            }
+        }
+        //從資源回收桶撈出剛剛丟的那一步
+        public void redo(){
+            if (recycle.size() > 1) { //有線才能拿
+                lines.add(recycle.removeLast());//倒回那一步，是從回收桶拿的
+                invalidate();//畫面清掉
+
+            }
+
+
+        }
         private class Point { //類別
 
             float x, y;
